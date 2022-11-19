6222275efaee3b2618fed0f7407a0aab50057d34
Mikael Freva <mikaelfreva@live.fr>
2022-11-19 02:20:34 +0100

    portfolio

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -8,6 +8,9 @@ interface HeaderStylesProps {
   toggle: boolean,
 }
 export const HeaderWrapper = styled.header<HeaderStylesProps>`
+*{
+  transition:all .3s cubic-bezier(0.19, 1, 0.22, 1);
+}
 .home_link{
   *{
     cursor: pointer;
