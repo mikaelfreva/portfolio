28801acc5d9d021a88ed967c002682762d3051c2
Mikael Freva <107946431+mikaelfreva@users.noreply.github.com>
2023-01-31 14:56:34 +0100

    Update globals.ts

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -8,10 +8,9 @@ const GlobalStyles = createGlobalStyle`
   ${normalize};
 body{
     background-image: ${({ theme }) => theme.background};
-  background-size:cover ;
   background-attachment: fixed;
     border: none;
-
+background-size: 100vmax 100vmax;
 }
 
 
